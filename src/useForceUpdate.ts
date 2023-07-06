0b229c74c40e1f8c0cbb2e71f2d761c9e56f11fc
Georg Kaindl <gkaindl@mac.com>
2023-07-06 10:52:08 +0200

    Shorten forceUpdate hook.

--- src/useForceUpdate.ts
+++ src/useForceUpdate.ts
@@ -1,24 +1,16 @@
-import { Reducer, useCallback, useEffect, useReducer, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export function useForceUpdate(): () => void {
-    const [, forceUpdate] = useReducer<Reducer<number, void>>((x) => x + 1, 0);
-    const mountState = useRef({ mounted: false, pending: false });
-    useEffect(() => {
-        mountState.current.mounted = true;
-        if (mountState.current.pending) {
-            mountState.current.pending = false;
-            forceUpdate();
-        }
-        return () => {
-            mountState.current = { mounted: false, pending: false };
-        };
-    }, []);
-    const update = useCallback(() => {
-        if (mountState.current.mounted) {
-            forceUpdate();
-        } else {
-            mountState.current.pending = true;
-        }
-    }, [mountState, forceUpdate]);
-    return update;
+	const [, setValue] = useState<undefined[]>([]);
+	const forceUpdateRef = useRef<(() => void) | undefined>(() => setValue([]));
+
+	useEffect(
+		() => () => { forceUpdateRef.current = undefined; },
+		[forceUpdateRef]
+	);
+
+	return useCallback(
+		() => forceUpdateRef.current?.(),
+		[forceUpdateRef]
+	);
 }
